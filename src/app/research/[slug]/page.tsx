fa6b805d900a66fcc9a0ea51e5a3346b7b9ccc81
Ace <acekisch@gmail.com>
2025-05-09 15:10:06 -0700

    post nav

--- src/app/research/[slug]/page.tsx
+++ src/app/research/[slug]/page.tsx
@@ -32,6 +32,7 @@ export default async function NewsPostPage({
       date={post.date}
       content={post.content}
       image={post.image}
+      slug={post.slug}
     />
   );
 }
--- src/components/Post/Page.tsx
+++ src/components/Post/Page.tsx
@@ -1,19 +1,28 @@
 "use client";
 import styles from "./Page.module.scss";
 import Image from "next/image";
+import Link from "next/link";
+import posts from "@/lib/posts.json";
+import ChevronLeft from "@/assets/icons/chevron-left.svg";
+import ChevronRight from "@/assets/icons/chevron-right.svg";
 
 interface PageProps {
+  slug: string;
   title: string;
   date: string;
   content: string;
   image: string;
 }
 
-const Page: React.FC<PageProps> = ({ title, date, content, image }) => {
+const Page: React.FC<PageProps> = ({ slug, title, date, content, image }) => {
+  const currentIndex = posts.findIndex((p) => p.slug === slug);
+  const prevPost = posts[currentIndex - 1];
+  const nextPost = posts[currentIndex + 1];
+
   return (
     <main className={styles.container}>
       <article className={styles.post}>
-        <div className={styles.image}>
+        <div className={styles.imageWrapper}>
           <Image
             src={image}
             alt={title}
@@ -33,6 +42,48 @@ const Page: React.FC<PageProps> = ({ title, date, content, image }) => {
           ))}
         </div>
       </article>
+
+      <div className="divider"/>
+
+      {(prevPost || nextPost) && (
+   
+          <nav className={styles.postNav}>
+            {prevPost && (
+              <Link
+                href={`/research/${prevPost.slug}`}
+                className={styles.prevItem}
+              >
+                <Image
+                  src={prevPost.image}
+                  alt={prevPost.title}
+                  width={180}
+                  height={84}
+                  className={styles.thumbnail}
+                />
+                <span className={styles.navTitle}>{prevPost.title}</span>
+                <span className={styles.prev}> <ChevronLeft />Prev Post</span>
+              </Link>
+            )}
+            {nextPost && (
+              <Link
+                href={`/research/${nextPost.slug}`}
+                className={styles.nextItem}
+              >
+              
+                <Image
+                  src={nextPost.image}
+                  alt={nextPost.title}
+                  width={180}
+                  height={84}
+                  className={styles.thumbnail}
+                />
+                <span className={styles.navTitle}>{nextPost.title}</span>
+                <span className={styles.next}>Next Post <ChevronRight /></span>
+              </Link>
+            )}
+          </nav>
+   
+      )}
     </main>
   );
 };
