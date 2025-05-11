a2c2cdaa35848b5ac0c2cf652478b605ed3e57b9
Ace <acekisch@gmail.com>
2025-05-11 11:08:15 -0700

    hero bg update

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,16 +1,26 @@
 import styles from "./styles.module.scss";
+import Image from "next/image";
 
 export default function Hero() {
   return (
     <section className={styles.hero}>
+      <div className={styles.imageWrapper}>
+        <Image
+          src="/images/black-mesa-new-mexico-contrast.jpg"
+          alt="Black Mesa Research Facility"
+          fill
+          priority
+          fetchPriority="high"
+          quality={100}
+          className={styles.image}
+        />
+      </div>
       <div className={styles.overlay} />
       <div className={styles.gradientFade} />
-      {/* <div className={styles.mask}> */}
-        <div className={styles.content}>
-          <h1>Black Mesa</h1>
-          <p>Research Facility</p>
-        </div>
-      {/* </div> */}
+      <div className={styles.content}>
+        <h1>Black Mesa</h1>
+        <p>Research Facility</p>
+      </div>
     </section>
   );
 }
